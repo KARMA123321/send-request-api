b022abe79278db7cfb95ac045792d103e0dc8375
d.antipov <7991ghost@gmail.com>
2023-10-08 13:35:10 +0300

    Add new company test and rewrite previous with new assert

--- tests/companies/companies_positive_tests.py
+++ tests/companies/companies_positive_tests.py
@@ -1,11 +1,15 @@
-import requests
-import config
+import pytest
 
 from src.responses.companies.get_companies_response import GetCompaniesResponse
-from src.responses.base_response import BaseResponse
-from tests.conftest import schemas
+from src.enums.company_statuses import CompanyStatuses
+from conftest import schemas
 
 
-def test_get_companies(schemas):
-    response = GetCompaniesResponse(params={"limit": 2})
-    response.validate_against_schema(schemas["CompanyList"])
+def test_get_companies_schema_and_status_code(schemas):
+    GetCompaniesResponse().assert_schema(schemas).assert_status_code(200)
+
+
+@pytest.mark.parametrize("status", [CompanyStatuses.ACTIVE, CompanyStatuses.BANKRUPT,
+                                    CompanyStatuses.CLOSED])
+def test_get_companies_filter_by_status(status):
+    GetCompaniesResponse(params={GetCompaniesResponse.STATUS_QUERY: status}).assert_companies_statuses(status)
