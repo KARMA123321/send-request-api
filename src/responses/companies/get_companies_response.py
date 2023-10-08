b3601fa26e1f9719c604513fcdb8a1aa200c3eb8
d.antipov <7991ghost@gmail.com>
2023-10-08 18:21:52 +0300

    Add some improvements
    Add useful companies attribute to GetCompaniesResponse class and move query params names out of the class to make life easier when it is necessary to use params in tests
    
    Refactor test_get_companies_filter_by_status

--- src/responses/companies/get_companies_response.py
+++ src/responses/companies/get_companies_response.py
@@ -8,14 +8,16 @@ from src.enums.schemas.schemas_keys.company import Company
 from src.enums.schemas.schemas_keys.company_list import CompanyList
 
 
-class GetCompaniesResponse(BaseResponse):
-    STATUS_QUERY = "status"
-    LIMIT_QUERY = "limit"
-    OFFSET_QUERY = "offset"
+STATUS_QUERY = "status"
+LIMIT_QUERY = "limit"
+OFFSET_QUERY = "offset"
 
+
+class GetCompaniesResponse(BaseResponse):
     def __init__(self, **kwargs):
         response = requests.get(config.FULL_URL + "/companies", **kwargs)
         super().__init__(response)
+        self.companies = self.response_json[CompanyList.Data]
 
     def assert_schema(self, schemas):
         print(f"Assert schema matches {SchemasNames.CompanyList}")
@@ -26,7 +28,7 @@ class GetCompaniesResponse(BaseResponse):
         print("Assert companies statuses")
 
         error = "Expected company status to be '{0}', but was '{1}'"
-        for company in self.response_json[CompanyList.Data]:
+        for company in self.companies:
             statuses_equality = company[Company.Status] == expected_status
             print(f"{company[Company.Name]} status equal to {expected_status} [{statuses_equality}]")
             assert statuses_equality, error.format(expected_status, company[Company.Status])
--- tests/companies/companies_positive_tests.py
+++ tests/companies/companies_positive_tests.py
@@ -1,6 +1,6 @@
 import pytest
 
-from src.responses.companies.get_companies_response import GetCompaniesResponse
+from src.responses.companies.get_companies_response import GetCompaniesResponse, STATUS_QUERY
 from src.enums.company_statuses import CompanyStatuses
 from conftest import schemas
 
@@ -9,7 +9,8 @@ def test_get_companies_schema_and_status_code(schemas):
     GetCompaniesResponse().assert_schema(schemas).assert_status_code(200)
 
 
-@pytest.mark.parametrize("status", [CompanyStatuses.ACTIVE, CompanyStatuses.BANKRUPT,
-                                    CompanyStatuses.CLOSED])
-def test_get_companies_filter_by_status(status):
-    GetCompaniesResponse(params={GetCompaniesResponse.STATUS_QUERY: status}).assert_companies_statuses(status)
+@pytest.mark.parametrize("company_status", [CompanyStatuses.ACTIVE, CompanyStatuses.BANKRUPT,
+                                            CompanyStatuses.CLOSED])
+def test_get_companies_filter_by_status(company_status):
+    GetCompaniesResponse(params={STATUS_QUERY: company_status})\
+        .assert_companies_statuses(company_status)
