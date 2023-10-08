59d082eee6747ad15c32ce3b2d41341a0a9cfe07
d.antipov <7991ghost@gmail.com>
2023-10-08 13:31:36 +0300

    Move conftest with schemas fixture to the root directory because it looks better

--- conftest.py
+++ conftest.py
@@ -4,7 +4,7 @@ import pytest
 import requests
 
 
-@pytest.fixture(scope="package")
+@pytest.fixture(scope="session")
 def schemas():
     spec = requests.get("https://send-request.me/openapi.json")
     deref_spec = JsonDeref().deref(spec.json(), 1000)
