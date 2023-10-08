e6a087fe32bd2f888bc07ae06431c123f8b28bef
d.antipov <7991ghost@gmail.com>
2023-10-08 13:34:14 +0300

    Add new asserts, query constants, attributes to response classes to use it in tests

--- src/responses/base_response.py
+++ src/responses/base_response.py
@@ -5,10 +5,17 @@ from requests import Response
 class BaseResponse:
     def __init__(self, response: Response):
         self.response = response
+        self.url = response.request.url
         self.status_code = response.status_code
+        self.reason = response.reason
         self.response_json = response.json()
 
+    def __str__(self): return f"Response {self.url} [{self.status_code} {self.reason}]"
+
+    def __repr__(self): return f"<{self.url} [{self.status_code} {self.reason}]>"
+
     def validate_against_schema(self, schema):
+        print(f"Validate response against JSON schema '{schema}'")
         if isinstance(self.response_json, list):
             for item in self.response_json:
                 validate(item, schema)
@@ -17,8 +24,10 @@ class BaseResponse:
 
     def assert_status_code(self, status_code):
         if isinstance(status_code, (list, tuple, set)):
+            print(f"Assert {self.status_code} status code in {status_code}")
             assert self.status_code in status_code
         else:
+            print(f"Assert {self.status_code} status code equal to {status_code}")
             assert self.status_code == status_code
 
         return self
--- src/responses/companies/get_companies_response.py
+++ src/responses/companies/get_companies_response.py
@@ -1,10 +1,32 @@
-from src.responses.base_response import BaseResponse
-
 import config
 import requests
 
+from src.responses.base_response import BaseResponse
+from jsonschema.validators import validate
+from src.enums.schemas.schemas_names import SchemasNames
+from src.enums.schemas.schemas_keys.company import Company
+from src.enums.schemas.schemas_keys.company_list import CompanyList
+
 
 class GetCompaniesResponse(BaseResponse):
+    STATUS_QUERY = "status"
+    LIMIT_QUERY = "limit"
+    OFFSET_QUERY = "offset"
+
     def __init__(self, **kwargs):
         response = requests.get(config.FULL_URL + "/companies", **kwargs)
         super().__init__(response)
+
+    def assert_schema(self, schemas):
+        print(f"Assert schema matches {SchemasNames.CompanyList}")
+        validate(self.response_json, schemas[SchemasNames.CompanyList])
+        return self
+
+    def assert_companies_statuses(self, expected_status):
+        print("Assert companies statuses")
+
+        error = "Expected company status to be '{0}', but was '{1}'"
+        for company in self.response_json[CompanyList.Data]:
+            statuses_equality = company[Company.Status] == expected_status
+            print(f"{company[Company.Name]} status equal to {expected_status} [{statuses_equality}]")
+            assert statuses_equality, error.format(expected_status, company[Company.Status])
