508e39be4588dfea7288445e848ccfc46fd0f29f
d.antipov <7991ghost@gmail.com>
2023-10-14 02:00:50 +0300

    Add pytest-check instead of my custom multiple assert. It does the same thing but looks better and have another great functions

--- tests/companies/companies_positive_tests.py
+++ tests/companies/companies_positive_tests.py
@@ -1,6 +1,7 @@
 import allure
 import pytest
 
+from pytest_check import check
 from src.asserts import Assert
 from src.enums.schemas.schemas_keys.company import Company
 from src.responses.companies.get_companies_response import GetCompaniesResponse, STATUS_QUERY, LIMIT_QUERY, OFFSET_QUERY
@@ -23,10 +24,10 @@ def test_get_companies_filter_by_status(company_status):
 @allure.title("GET /api/companies with limit '{value}' and offset '{value}'")
 @pytest.mark.parametrize("value", [0, 3])
 def test_get_companies_filter_by_limit_and_offset(value):
-    Assert.multiple(
-        lambda: GetCompaniesResponse(params={LIMIT_QUERY: value}).validate.companies_amount_equal_to_limit(value),
-        lambda: GetCompaniesResponse(params={OFFSET_QUERY: value}).validate.companies_start_from_offset(value)
-    )
+    with check:
+        GetCompaniesResponse(params={LIMIT_QUERY: value}).validate.companies_amount_equal_to_limit(value)
+    with check:
+        GetCompaniesResponse(params={OFFSET_QUERY: value}).validate.companies_start_from_offset(value)
 
 
 @allure.title("GET /api/companies/{company_id} schema and status code are valid")
